ed104ef903d0b6f2d1c3e9de6da496c6ac3ec7d7
nirajsam <nirajsam15@gmail.com>
2024-11-18 01:12:20 +0530

    updated BE vercel url

--- my_quiz/src/config.js
+++ my_quiz/src/config.js
@@ -1,6 +1,6 @@
 
 module.exports={
-    URL: 'https://niraj-quiz-app.herokuapp.com', 
+    URL: 'https://quiz-app-with-86yuvlz8x-nirajsams-projects.vercel.app', 
     // URL: 'http://localhost:5001'
     //  
 }
