e28a5948500024a9185d26de41b45f076ee148aa
nirajsam <nirajsam15@gmail.com>
2024-11-18 23:54:08 +0530

    BE url change

--- my_quiz/src/config.js
+++ my_quiz/src/config.js
@@ -1,6 +1,6 @@
 
 module.exports={
-    URL: 'https://quiz-app-with-86yuvlz8x-nirajsams-projects.vercel.app', 
+    URL: 'https://quiz-app-with-o0kyliraf-nirajsams-projects.vercel.app', 
     // URL: 'http://localhost:5001'
     //  
 }
